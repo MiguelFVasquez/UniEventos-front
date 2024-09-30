a955835af6ef7e7a8a78462f1ec0d1bb8fadcb12
Miguel Florez <florezvasquezjuanmiguel@gmail.com>
2024-09-29 21:56:22 -0500

    Admin dashBoard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,23 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './log-in/log-in.component';
 import { RegistroComponent } from './registro/registro.component';
+import { AdminHeaderComponent } from './admin-header/admin-header.component';
+import { AdminDashBoardComponent } from './admin-dash-board/admin-dash-board.component';
 
 export const routes: Routes = [
     { path: 'log-in', component: LoginComponent },
     { path: 'registro', component: RegistroComponent },
 
+    //Ruta anindada
+    {
+      path: 'admin',
+      component: AdminHeaderComponent, // Muestra el AdminHeader siempre
+      children: [
+        { path: 'dashboard', component: AdminDashBoardComponent }, // AdminDashBoard como hijo
+      ]
+    },
 
+    { path: '', redirectTo: '/log-in', pathMatch: 'full' },
+    { path: '**', redirectTo: '/log-in' }, 
 
-];
+];  
--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { RegistroComponent } from '../registro/registro.component';
+import { InitialHeaderComponent } from '../initial-header/initial-header.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule,FormsModule,RegistroComponent], 
+  imports: [RouterModule,FormsModule,RegistroComponent, InitialHeaderComponent], 
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css',
   
@@ -18,13 +19,20 @@ export class LoginComponent {
 
   constructor(private router: Router) {}
 
-  onSubmit() {
-    console.log('Email:', this.email);
-    console.log('Password:', this.password);
-    // Aquí puedes agregar la lógica para autenticar al usuario
-  }
-
+  // Alternar visibilidad de la contraseña
   togglePasswordVisibility() {
     this.passwordVisible = !this.passwordVisible;
+  }
+
+  // Método que se llama cuando se envía el formulario
+  onSubmit() {
+    const validEmail = 'admin@gmail.com';
+    const validPassword = 'admin';
+
+    if (this.email === validEmail && this.password === validPassword) {
+      this.router.navigate(['/admin/dashboard']); // Redirige al dashboard
+    } else {
+      alert('Correo electrónico o contraseña incorrectos.');
+    }
   }
 }
