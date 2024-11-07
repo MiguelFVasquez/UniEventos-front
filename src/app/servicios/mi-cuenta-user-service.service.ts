056a767dfeb281affa6b7be356b63a8cb1d982c0
Miguel Florez <florezvasquezjuanmiguel@gmail.com>
2024-11-06 22:32:42 -0500

    Acceso a datos de cuenta cliente

--- src/app/servicios/mi-cuenta-user-service.service.ts
+++ src/app/servicios/mi-cuenta-user-service.service.ts
@@ -4,23 +4,21 @@ import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { MessageDTO } from '../models/message.dto';
 import { AuthService } from '../servicios/auth.service'; 
-import { map, tap } from 'rxjs/operators';
 import { InfoAdicionalDTO } from '../models/InfoAdicionalDTO';
 @Injectable({
   providedIn: 'root'
 })
-export class MiCuentaService {
-  private apiUrl= 'http://localhost:8080/api/cuenta'; //url para el controller del admin
+export class MiCuentaUserServiceService {
+  private apiUrl= 'http://localhost:8080/api/cuenta/user'; //url para el controller del user
   constructor(private http: HttpClient, private authService: AuthService,private router: Router) { }
 
-  //----------------------METODOS PARA EL MANEJO DE LA CUENTA DEL ADMIN-------------------------
-  //Metodo 
-  eliminarCuenta(email: string): Observable<MessageDTO> {
+
+  eliminarCuentaUser(email:string):Observable<MessageDTO>{
     const token = this.authService.getToken(); // Obtener el token del servicio de autenticación
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`); // Agregar el token a los encabezados
     return this.http.delete<MessageDTO>(`${this.apiUrl}/eliminar-cuenta/${email}`, { headers });
   }
-  editarCuenta(infoCuenta:InfoAdicionalDTO) :Observable<MessageDTO> {
+  editarCuentaUser(infoCuenta:InfoAdicionalDTO) :Observable<MessageDTO> {
     const token = this.authService.getToken(); // Obtener el token del servicio de autenticación
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`); // Agregar el token a los encabezados
     return this.http.put<MessageDTO>(`${this.apiUrl}/editar-cuenta`, infoCuenta, { headers });
