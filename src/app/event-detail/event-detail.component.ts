5d985af56d6e693987f3486508a54e852712e020
Juanex <juane.cruzp@uqvirtual.edu.co>
2024-11-09 21:47:18 -0500

    Evento detalle parcialmente funcionando

--- src/app/event-detail/event-detail.component.ts
+++ src/app/event-detail/event-detail.component.ts
@@ -1,13 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderInicioPrincipalComponent } from '../header-inicio-principal/header-inicio-principal.component';
+import { ActivatedRoute,Router  } from '@angular/router';
+import { Evento } from '../models/evento'; // Asegúrate de que la ruta sea correcta
+import { CommonModule } from '@angular/common';
+import { EventoService } from '../servicios/evento-service.service';
+import { MensajeDTO }  from '../models/mensaje-dto';
 
 @Component({
   selector: 'app-event-detail',
   standalone: true,
-  imports: [HeaderInicioPrincipalComponent],
+  imports: [HeaderInicioPrincipalComponent, CommonModule],
   templateUrl: './event-detail.component.html',
   styleUrl: './event-detail.component.css'
 })
-export class EventDetailComponent {
+
+export class EventDetailComponent implements OnInit{
+  evento: Evento= {
+    id: '',
+    nombre: '', 
+    descripcion: '',
+    direccion: '',
+    ciudad: '',
+    fecha: new Date,
+    estado: '',
+    tipo: '',
+    imagenPortada: '',
+    imagenLocalidades: '',
+    localidades: [],
+    promedioCalificaciones: 0
+  }
+  constructor(  private route: ActivatedRoute,
+    private eventoService: EventoService,
+    private router: Router){
   
+  }
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id'); // Asegúrate de usar 'id'
+    if (id) {
+      this.obtenerEvento(id); // Llamar a la función para obtener el evento
+    } else {
+      console.error('ID del evento no encontrado en la ruta');
+    }  
+  }
+    obtenerEvento(id: string): void {
+      this.eventoService.getEventoById(id).subscribe(
+        (data: Evento) => {
+          // Convierte la fecha al tipo Date para asegurar la visualización correcta
+          this.evento = {
+            ...data,
+            fecha: new Date(data.fecha) // convierte la fecha recibida a un objeto Date
+          };
+        },
+        (error) => {
+          console.error('Error al obtener el evento:', error);
+        }
+      );
+    }
 }
